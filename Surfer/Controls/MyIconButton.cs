f144271368a46f69efd7773900feffecb965df16
Ozay Akcan <mail@ozayakcan.com.tr>
2023-06-09 13:04:14 +0300

    Add VisualDisabled property to MyIconButton.

--- Surfer/Controls/MyIconButton.cs
+++ Surfer/Controls/MyIconButton.cs
@@ -6,10 +6,44 @@ namespace Surfer.Controls
 {
     public partial class MyIconButton: IconButton
     {
+        private bool _visualDisabled = false;
+        private Color DefaultMouseDownBackColor;
+        private Color DefaultMouseOverBackColor;
+        public bool VisualDisabled
+        {
+            get
+            {
+                return _visualDisabled;
+            }
+            set
+            {
+                _visualDisabled = value;
+                if (value)
+                {
+                    FlatAppearance.MouseDownBackColor = FlatAppearance.MouseOverBackColor = BackColor;
+                }
+                else
+                {
+                    FlatAppearance.MouseDownBackColor = DefaultMouseDownBackColor;
+                    FlatAppearance.MouseOverBackColor = DefaultMouseOverBackColor;
+                }
+            }
+        }
+        private bool ShouldSerializeVisualDisabled()
+        {
+            return _visualDisabled;
+        }
+        private void ResetVisualDisabled()
+        {
+            VisualDisabled = false;
+        }
+
         public MyIconButton(): base()
         {
             FlatAppearance.BorderSize = 0;
-            BackColor = FlatAppearance.MouseOverBackColor = Color.White;
+            DefaultMouseDownBackColor = FlatAppearance.MouseDownBackColor;
+            DefaultMouseOverBackColor = FlatAppearance.MouseOverBackColor;
+            BackColor  = Color.White;
             FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             SetStyle(ControlStyles.Selectable, false);
         }
