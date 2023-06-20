c29a1002dc64407ee3768d87431a5e4952268282
Ozay Akcan <mail@ozayakcan.com.tr>
2023-06-20 15:37:51 +0300

    Updated key events.

--- Surfer/BrowserSettings/MyKeyboardHandler.cs
+++ Surfer/BrowserSettings/MyKeyboardHandler.cs
@@ -18,15 +18,13 @@ namespace Surfer.BrowserSettings
         public bool OnKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
         {
             Debug.WriteLine("Key Codes: " + windowsKeyCode.ToString() + ", " + nativeKeyCode.ToString());
-            var chBrowser = (ChromiumWebBrowser)chromiumWebBrowser;
-            
-            
+            if (type == KeyType.KeyUp)
+                MyBrowser.KeyEvents((ChromiumWebBrowser)chromiumWebBrowser, modifiers, (Keys)windowsKeyCode);
             return false;
         }
 
         public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
         {
-            MyBrowser.KeyEvents((ChromiumWebBrowser)chromiumWebBrowser, modifiers, (Keys)windowsKeyCode);
             return false;
         }
     }
--- Surfer/Forms/MyAppContainer.cs
+++ Surfer/Forms/MyAppContainer.cs
@@ -5,11 +5,13 @@ using Surfer.BrowserSettings;
 
 namespace Surfer.Forms
 {
-    public partial class MyAppContainer : TitleBarTabs
+    public partial class MyAppContainer : TitleBarTabs, IMessageFilter
     {
         public MyAppContainer()
         {
             InitializeComponent();
+            Application.AddMessageFilter(this);
+            this.FormClosed += (s, e) => Application.RemoveMessageFilter(this);
             AeroPeekEnabled = true;
             TabRenderer = new ChromeTabRenderer(this);
             TabSelected += MyAppContainer_TabSelected;
@@ -34,10 +36,22 @@ namespace Surfer.Forms
             };
             return titlebarTab;
         }
+        bool mRepeating;
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
-            Browser myBrowser = (Browser)SelectedTab.Content;
-            return myBrowser.KeyEvents(myBrowser.chBrowser, CefEventFlags.None, keyData, base.ProcessCmdKey(ref msg, keyData));
+            if (!mRepeating)
+            {
+                mRepeating = true;
+                Browser myBrowser = (Browser)SelectedTab.Content;
+                return myBrowser.KeyEvents(myBrowser.chBrowser, CefEventFlags.None, keyData, base.ProcessCmdKey(ref msg, keyData));
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x101) mRepeating = false;
+            return false;
         }
     }
 }
